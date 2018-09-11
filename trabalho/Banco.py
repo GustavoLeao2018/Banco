6214d932fdc321fc7b2101737a328354628cf32b
GustavoLeao2018 <glno181510004>
2018-09-11 09:52:51 -0300

    Criada a conexão com o banco de dados e tabela de produtos.

---
+++ trabalho/Banco.py
@@ -0,0 +1,28 @@
+#!/bin/env python3
+"""Banco de dados de um estoque."""
+from sqlite3 import *
+
+
+class Banco:
+    def conectar(self):
+        return connect("banco.db")
+    def fechar(self, db):
+        db.close()
+
+    def tabela(self):
+        sql = """
+            CREATE TABLE IF NOT EXISTS produto(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                tipo TEXT NOT NULL,
+                preco_unitario REAL NOT NULL,
+                produto TEXT NOT NULL,
+                quantidade INTEGER NOT NULL
+            );
+        """
+        self.db = self.conectar()
+        self.cursor = self.db.cursor()
+        self.cursor.execute(sql)
+        self.fechar(self.db)
+
+    def __init__(self):
+        self.tabela()
